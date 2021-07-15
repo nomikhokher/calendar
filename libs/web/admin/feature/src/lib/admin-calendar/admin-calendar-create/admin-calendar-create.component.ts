ab66a0028f79f742ee9748c88db74e6ca6d2d0bf
Noman Khokher <nomankhokher786@gmail.com>
2021-07-16 00:35:30 +0500

    admin/calendars/create add fields

--- libs/web/admin/feature/src/lib/admin-calendar/admin-calendar-create/admin-calendar-create.component.ts
+++ libs/web/admin/feature/src/lib/admin-calendar/admin-calendar-create/admin-calendar-create.component.ts
@@ -9,14 +9,12 @@ import { AdminCalendarCreateStore } from './admin-calendar-create.store'
       <div class="dark:bg-gray-800 px-6 py-4 rounded-md">
         <div class="flex flex-row">
           <div class="flex w-full"></div>
-          <div class="flex-none space-x-2">
-            <ui-button label="Submit" type="submit"></ui-button>
-          </div>
+          <div class="flex-none space-x-2"></div>
         </div>
         <ui-form [fields]="fields" [model]="{}" (submitForm)="createCalendar($event)">
           <div class="flex flex-row">
             <div class="flex w-full"></div>
-            <div class="flex-none space-x-2">
+            <div class="flex-none space-x-2 m-8">
               <ui-button label="Submit" type="submit"></ui-button>
             </div>
           </div>
@@ -33,11 +31,40 @@ export class AdminCalendarCreateComponent {
     WebUiFormField.fieldRow([
       WebUiFormField.input('id', { label: 'Id' }, { className: 'w-1/2  px-1', hide: true }),
       WebUiFormField.input('title', { label: 'Title' }, { className: 'w-1/2  px-1' }),
+      WebUiFormField.input('color', { label: 'Color' }, { className: 'w-1/2  px-1' }),
     ]),
     WebUiFormField.fieldRow([
-      WebUiFormField.input('color', { label: 'Color' }, { className: 'w-1/2  px-1' }),
-      WebUiFormField.input('visible', { label: 'Visible' }, { className: 'w-1/2  px-1' }),
+      WebUiFormField.input('id', { label: 'Id' }, { className: 'w-1/2  px-1', hide: true }),
+      WebUiFormField.input('calendarId', { label: 'Calendar Id' }, { className: 'w-1/2  px-1', hide: true }),
     ]),
+    WebUiFormField.fieldRow([
+      WebUiFormField.input('visible', { label: 'Visible' }, { className: 'w-1/2  px-1' }),
+      WebUiFormField.input(
+        'recurringEventId',
+        { label: 'Recurring Event Id' },
+        { className: 'w-1/2  px-1', hide: true },
+      ),
+      WebUiFormField.input('isFirstInstance', { label: 'Is First Instance' }, { className: 'w-1/2  px-1' }),
+    ]),
+    WebUiFormField.fieldRow([
+      WebUiFormField.input('title', { label: 'Title' }, { className: 'w-1/2  px-1' }),
+      WebUiFormField.input('description', { label: 'Description' }, { className: 'w-1/2  px-1' }),
+    ]),
+    WebUiFormField.fieldRow([
+      WebUiFormField.input('start', { label: 'Start' }, { className: 'w-1/2  px-1' }),
+      WebUiFormField.input('end', { label: 'End' }, { className: 'w-1/2  px-1' }),
+    ]),
+    WebUiFormField.fieldRow([
+      WebUiFormField.input('allDay', { label: 'All Day' }, { className: 'w-1/2  px-1' }),
+      WebUiFormField.input('recurrence', { label: 'Recurrence' }, { className: 'w-1/2  px-1' }),
+    ]),
+    WebUiFormField.typeahead('calendarId', {
+      placeholder: 'Calendar',
+      items: [],
+      onSearch: (searchTerm) => {
+        return []
+      },
+    }),
   ]
   constructor(private readonly store: AdminCalendarCreateStore) {}
 
