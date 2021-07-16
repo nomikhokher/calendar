cd8b4b3b0d2e73ae0d591b78364d186a00e0ff20
Noman Khokher <nomankhokher786@gmail.com>
2021-07-16 18:09:14 +0500

    revert changes

--- libs/web/admin/feature/src/lib/admin-calendar/admin-calendar-create/admin-calendar-create.component.ts
+++ libs/web/admin/feature/src/lib/admin-calendar/admin-calendar-create/admin-calendar-create.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core'
 import { WebUiFormField } from '@calendar/web/ui/form'
 import { AdminCalendarCreateStore } from './admin-calendar-create.store'
-
 @Component({
   template: `
     <ng-container *ngIf="vm$ | async as vm">
@@ -9,12 +8,11 @@ import { AdminCalendarCreateStore } from './admin-calendar-create.store'
       <div class="dark:bg-gray-800 px-6 py-4 rounded-md">
         <div class="flex flex-row">
           <div class="flex w-full"></div>
-          <div class="flex-none space-x-2"></div>
         </div>
         <ui-form [fields]="fields" [model]="{}" (submitForm)="createCalendar($event)">
           <div class="flex flex-row">
             <div class="flex w-full"></div>
-            <div class="flex-none space-x-2 m-8">
+            <div class="flex-none space-x-2">
               <ui-button label="Submit" type="submit"></ui-button>
             </div>
           </div>
@@ -26,48 +24,15 @@ import { AdminCalendarCreateStore } from './admin-calendar-create.store'
 })
 export class AdminCalendarCreateComponent {
   readonly vm$ = this.store.vm$
-
   fields = [
     WebUiFormField.fieldRow([
       WebUiFormField.input('id', { label: 'Id' }, { className: 'w-1/2  px-1', hide: true }),
       WebUiFormField.input('title', { label: 'Title' }, { className: 'w-1/2  px-1' }),
-      WebUiFormField.input('color', { label: 'Color' }, { className: 'w-1/2  px-1' }),
     ]),
-    WebUiFormField.fieldRow([
-      WebUiFormField.input('id', { label: 'Id' }, { className: 'w-1/2  px-1', hide: true }),
-      WebUiFormField.input('calendarId', { label: 'Calendar Id' }, { className: 'w-1/2  px-1', hide: true }),
-    ]),
-    WebUiFormField.fieldRow([
-      WebUiFormField.input('visible', { label: 'Visible' }, { className: 'w-1/2  px-1' }),
-      WebUiFormField.input(
-        'recurringEventId',
-        { label: 'Recurring Event Id' },
-        { className: 'w-1/2  px-1', hide: true },
-      ),
-      WebUiFormField.input('isFirstInstance', { label: 'Is First Instance' }, { className: 'w-1/2  px-1' }),
-    ]),
-    WebUiFormField.fieldRow([
-      WebUiFormField.input('title', { label: 'Title' }, { className: 'w-1/2  px-1' }),
-      WebUiFormField.input('description', { label: 'Description' }, { className: 'w-1/2  px-1' }),
-    ]),
-    WebUiFormField.fieldRow([
-      WebUiFormField.input('start', { label: 'Start' }, { className: 'w-1/2  px-1' }),
-      WebUiFormField.input('end', { label: 'End' }, { className: 'w-1/2  px-1' }),
-    ]),
-    WebUiFormField.fieldRow([
-      WebUiFormField.input('allDay', { label: 'All Day' }, { className: 'w-1/2  px-1' }),
-      WebUiFormField.input('recurrence', { label: 'Recurrence' }, { className: 'w-1/2  px-1' }),
-    ]),
-    WebUiFormField.typeahead('calendarId', {
-      placeholder: 'Calendar',
-      items: [],
-      onSearch: (searchTerm) => {
-        return []
-      },
-    }),
+    WebUiFormField.fieldRow([WebUiFormField.input('color', { label: 'Color' }, { className: 'w-1/2  px-1' })]),
+    WebUiFormField.fieldRow([WebUiFormField.checkbox('visible', { label: 'Visible' }, { className: 'w-1/2  px-1' })]),
   ]
   constructor(private readonly store: AdminCalendarCreateStore) {}
-
   createCalendar(input) {
     this.store.createCalendarEffect(input)
   }
